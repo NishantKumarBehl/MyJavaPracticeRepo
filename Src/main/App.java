575d8d6dafb3ebf75386389b79e12b66b7bcfe7f
Nishant kumar  Behl <nrbehl@gmail.com>
2020-08-25 13:34:20 +0530

    New Class

---
+++ Src/main/App.java
@@ -0,0 +1,13 @@
+package com.Nishant.Tests;
+
+        public class TestingClass {
+
+
+        public static void main(String[] args){
+
+            System.out.println("Wow this is my fist class");
+
+        }
+
+
+        }
